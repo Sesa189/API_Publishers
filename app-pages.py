5529d8a4c1ac482415278e0b95c82cb1a2efb019
CesareNappa <cesare.nappa@fermi.mo.it>
2025-11-22 10:54:40 +0100

    Aggiunto metodo post publishers handler

--- app-pages.py
+++ app-pages.py
@@ -26,6 +26,25 @@ class PublishersHandler(tornado.web.RequestHandler):
                 doc["_id"] = str(doc["_id"])
                 self.write(doc)
 
+    async def post(self):
+        self.set_header("Content-Type", "application/json")
+        data = tornado.escape.json_decode(self.request.body)
+
+        if data["name"] and data["founded_year"] and data["country"]:
+            doc = {
+                "name": data["name"],
+                "founded_year": data["founded_year"],
+                "country": data["country"]
+            }
+            result = await publishers_collection.insert_one(doc)
+            doc["_id"] = str(result.inserted_id)
+            self.set_status(201)
+            self.write(doc)
+
+        else:
+            self.set_status(400)
+            self.write("JSON fornito non valido.")
+
 
 class BooksHandler(tornado.web.RequestHandler):
     async def get(self):
